7c22471fc1b8bbddcdfb47f104ca29486f19454c
limitlessfractals <fanwuq@gmail.com>
2018-01-26 18:02:18 -0500

    search bar

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Search from './src/Search';
 
 export default class App extends React.Component {
   state = {
-    currentScreen: "landing"
+    currentScreen: "search"
   }
   switchScreen = (currentScreen) =>{
     this.setState({currentScreen});
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Header, Item, Icon, Input, Button} from 'native-base';
 
 class Search extends React.Component{
+   state = {
+      pokeSearch: ""
+   }
+   searchPoke = () =>{
+
+   }
    render(){
       return(
-         <View>
-
+         <View style={{flex:1}}>
+            <Header
+               searchBar
+               rounded
+            >
+               <Item>
+                  <Icon name='ios-search' onPress={this.searchPoke}/>
+                  <Input
+                  value={this.state.pokeSearch}
+                  placeholder="Search Pokémon"
+                  onChangeText={(pokeSearch)=>this.setState({pokeSearch})}
+               />
+               </Item>               
+            </Header>
          </View>
       )
    }

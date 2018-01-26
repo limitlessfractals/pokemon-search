6d05b95c4a6567ecbde8076977334452b6b5e349
limitlessfractals <fanwuq@gmail.com>
2018-01-26 07:13:58 -0500

    native-base added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {  Text, View, Platform } from 'react-native';
+import {Button} from 'native-base';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button>Hello World</Button>
       </View>
     );
   }

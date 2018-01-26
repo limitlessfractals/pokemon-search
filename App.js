c114295e533340e19dfd23533bbf0c8d9a020ed6
limitlessfractals <fanwuq@gmail.com>
2018-01-26 08:04:03 -0500

    landing page

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import {  Text, View, Platform } from 'react-native';
-import {Button} from 'native-base';
+import {Text, View, Platform, ImageBackground } from 'react-native';
+import { Button } from 'native-base';
+
+var myBackground = require('./assets/icons/landing.jpg')
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Button>Hello World</Button>
+        <ImageBackground source={myBackground} style={styles.background}>
+          <View style={styles.viewStyle}>
+            <Text
+              style={styles.titleStyle}
+            >Welcome to Pokémon Search</Text>
+            <Button
+              block={true}
+              style={styles.buttonStyle}
+              onPress={()=>{}}
+            >
+              <Text style={styles.buttonText}>Search Now!</Text>
+            </Button>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
@@ -17,5 +31,30 @@ const styles = {
   container:{
     flex: 1,
     marginTop: Platform.OS === "android" ? 24 : 0
+  },
+  background: {
+    flex: 1,
+    width: null,
+    height: null
+  },
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleStyle: {
+    fontSize: 30,
+    color: 'blue',
+    alignItems: 'center',
+    textAlign: 'center',
+    fontFamily: Platform.OS === "ios" ? 'Chalkboard SE' : 'normal'
+  },
+  buttonStyle: {
+    margin: 10,
+  },
+  buttonText: {
+    color: 'white'
   }
 }
+
